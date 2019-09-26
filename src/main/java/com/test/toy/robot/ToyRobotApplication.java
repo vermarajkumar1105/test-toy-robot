cafeafa9b4c76c4488fc4fb591a98ffd309424bf
raj <vermarajkumar1105@gmail.com>
2019-09-26 14:32:28 +0530

    Test Toy Robot Application

--- src/main/java/com/test/toy/robot/ToyRobotApplication.java
+++ src/main/java/com/test/toy/robot/ToyRobotApplication.java
@@ -2,11 +2,13 @@ package com.test.toy.robot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import lombok.extern.slf4j.Slf4j;
-@Slf4j
 @SpringBootApplication
 public class ToyRobotApplication {
+	
+	private final static Logger log = LoggerFactory.getLogger(ToyRobotApplication.class);
 
     public static void main(String[] args) {
     	log.info("Spring Boot Toy Robot Application Loading...");
